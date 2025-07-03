dd762422307f3a8bf55a35fe80dd64d38310f3bf
Marcel Rösler <me@marcel.codes>
2025-07-03 10:23:24 +0200

    fix jq calls

--- addon-boomberman/rootfs/start/entrypoint.sh
+++ addon-boomberman/rootfs/start/entrypoint.sh
@@ -10,12 +10,11 @@ mkdir -p $SHARE_DATA_PATH
 CONFIG_PATH=/data/options.json
 
 cp $CONFIG_PATH $SHARE_DATA_PATH/options.json
-cat $CONFIG_PATH
 
 # Read configuration into variables
-ai_enabled=$(jq --raw-output '["AI Enabled"]' $CONFIG_PATH)
-ollama_url=$(jq --raw-output '["Ollama URL"]' $CONFIG_PATH)
-ollama_model=$(jq --raw-output '["Ollama model"]' $CONFIG_PATH)
+ai_enabled=$(jq --raw-output '.["AI Enabled"]' $CONFIG_PATH)
+ollama_url=$(jq --raw-output '.["Ollama URL"]' $CONFIG_PATH)
+ollama_model=$(jq --raw-output '.["Ollama model"]' $CONFIG_PATH)
 
 # Build final JSON object with structured logins
 json_string=$(jq -n \
@@ -28,15 +27,12 @@ json_string=$(jq -n \
     ollama_model: $ollama_model,
   }')
 
-echo "${json_string}"
-
 # render .env file config related files 
 echo "${json_string}" | tempio -template /templates/.env_template -out /.env
 cp /.env /app/.env
 
 cp /fakeResponses.json $SHARE_DATA_PATH/fakeResponses.json
 
-cat /app/.env
 . /app/.env
 
 echo "Starting Boomberman"
